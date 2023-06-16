67f3f0f42d71d5e9560093cab39181fc47b0daa2
Mohamed <yass.guelzim@gmail.com>
2023-06-16 09:52:01 -0700

    Tasks

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,7 +8,7 @@
  * main - prints the last digit of the random
  * number stored in the variable n
  *
- * Return: Always 0
+ * Return: Always 0 (Success)
  */
 
 int main(void)
@@ -29,11 +29,11 @@ int main(void)
 
 	else if (n % 10 == 0)
 
-	printf("and  is 0\n");
+	printf("and is 0\n");
 
 	else if (n % 10 < 6 && n % 10 != 0)
 
-	printf("and is less than and not 0\n");
+	printf("and is less than 6 and not 0\n");
 
 	return (0);
 
