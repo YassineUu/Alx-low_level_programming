1aa371a1bd595b42de10de5bc12e6941fb960936
Mohamed <yass.guelzim@gmail.com>
2023-06-22 05:02:02 -0700

    Tasks

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,13 +6,13 @@
  */
 void more_numbers(void)
 {
-	int i,j;
+	int i, j;
 
-	for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
 	{
-		for(j = 0; j <= 14; j++)
+		for (j = 0; j < 15; j++)
 		{
-			if(j >= 10)				
+			if (j >= 10)
 				_putchar(j / 10 + '0');
 			_putchar(j % 10 + '0');
 		}
