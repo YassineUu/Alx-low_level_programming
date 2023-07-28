74fb9273c221d30006cec797425104c47b64f0e7
YassineUu <yass.guelzim@gmail.com>
2023-07-27 18:54:16 -0700

    tasks

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main_bis - function that runs before main
+ * premain - function that runs before main
  *
  * Return: nothing
  */
 
-void __attribute__ ((constructor)) main_bis()
+void __attribute__ ((constructor)) premain()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf(" I bore my house upon my back:\n");
